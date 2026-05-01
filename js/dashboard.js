176a7576f1e0df37ae5d461490f85c9dd5b338a2
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2026-05-01 11:44:34 +0500

    Change name of backup file and fix the toggle width

--- js/dashboard.js
+++ js/dashboard.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(AppState));
         const downloadAnchorNode = document.createElement('a');
         downloadAnchorNode.setAttribute("href", dataStr);
-        downloadAnchorNode.setAttribute("download", "ProManager_Backup.json");
+        downloadAnchorNode.setAttribute("download", "AddOns-Manager_Backup.json");
         document.body.appendChild(downloadAnchorNode);
         downloadAnchorNode.click();
         downloadAnchorNode.remove();
